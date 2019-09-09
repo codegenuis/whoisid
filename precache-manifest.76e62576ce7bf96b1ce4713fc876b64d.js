c4190ee2242b771729e05a8b8e6ed4cda0d5bb3a
charles <charles@invoizpaid.com>
2019-09-09 11:17:28 +0100

    Updates

--- precache-manifest.76e62576ce7bf96b1ce4713fc876b64d.js
+++ precache-manifest.76e62576ce7bf96b1ce4713fc876b64d.js
@@ -1,6 +1,6 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "b314f8b784cb3c514d0f12d87547eca3",
+    "revision": "831580ba0fb655c2e6e45ecb83dd5443",
     "url": "/whoisid/index.html"
   },
   {
@@ -8,7 +8,7 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/whoisid/static/css/2.f0405d8a.chunk.css"
   },
   {
-    "revision": "7627934cbdb5b3607229",
+    "revision": "8d33c919a71b3092bd3f",
     "url": "/whoisid/static/css/main.3597c764.chunk.css"
   },
   {
@@ -16,8 +16,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/whoisid/static/js/2.2724718b.chunk.js"
   },
   {
-    "revision": "7627934cbdb5b3607229",
-    "url": "/whoisid/static/js/main.df3a7772.chunk.js"
+    "revision": "8d33c919a71b3092bd3f",
+    "url": "/whoisid/static/js/main.9855950b.chunk.js"
   },
   {
     "revision": "a1c7fff12bc481e608cf",
@@ -28,7 +28,7 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/whoisid/static/media/background-edit.4b34440f.jpg"
   },
   {
-    "revision": "86a4ef24aee3cfb7efc397a7e90acb68",
-    "url": "/whoisid/static/media/logo.86a4ef24.png"
+    "revision": "245d68c776cc5ae65e6279fb5cef85b7",
+    "url": "/whoisid/static/media/logo.245d68c7.png"
   }
 ]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/whoisid/precache-manifest.5dd3f3926bb657d2b65d04254c3b7132.js"
+  "/whoisid/precache-manifest.76e62576ce7bf96b1ce4713fc876b64d.js"
 );
 
 self.addEventListener('message', (event) => {
